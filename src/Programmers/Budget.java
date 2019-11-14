a71e32d6eb261b45567d88b08cde862dbcb29d0b
KimJuhyun92 <juhyun7793@gmail.com>
2019-11-14 19:54:39 +0900

    add budget

--- src/Programmers/Budget.java
+++ src/Programmers/Budget.java
@@ -1,26 +1,48 @@
 package Programmers;
+
 import java.util.Arrays;
 
 public class Budget {
-    class Solution {
-        public int solution(int[] budgets, int M) {
-            int answer = 0;
+    public int solution(int[] budgets, long M) {
 
-            Arrays.sort(budgets);
+        int answer = 0;
 
-            for(int i=0;i<budgets.length;i++){
-                if(budgets[i]>M/(budgets.length-i)){
-                    answer = M/(budgets.length-i);
-                    break;
-                }
-                M -= budgets[i];
-            }
-            if(answer==0){
-                answer=budgets[budgets.length-1];
-            }
+        Arrays.sort(budgets);
 
+        long sum = 0;
 
-            return answer;
+        for (int i = 0; i < budgets.length; i++) {
+            sum += budgets[i];
         }
+
+        int start = 0;
+        int end = budgets[budgets.length - 1];
+        int mid = (start + end) / 2;
+
+        while (!(start > mid)) {
+            sum = 0;
+
+            for (int i = 0; i < budgets.length; i++) {
+                if (mid < budgets[i])
+                    sum += mid;
+                else
+                    sum += budgets[i];
+            }
+
+            if (sum == M) {
+                answer = mid;
+                break;
+            } else if (sum > M) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+                answer = mid;
+            }
+
+            mid = (end + start) / 2;
+        }
+
+        return answer;
     }
+
 }
--- src/Programmers/Land124.java
+++ src/Programmers/Land124.java
@@ -1,24 +1,21 @@
 package Programmers;
 
 public class Land124 {
-    class Solution {
-        public String solution(int n) {
-            String answer = "";
 
-            while(n>0){
+    public String solution(int n) {
+        String answer = "";
 
-                if(n%3 == 0){
-                    answer = "4"+answer;
-                    n = n/3;
-                    n-=1;
-                } else{
-                    answer = n%3 + answer;
-                    n = n/3;
-                }
+        while (n > 0) {
 
+            if (n % 3 == 0) {
+                answer = "4" + answer;
+                n = n / 3;
+                n -= 1;
+            } else {
+                answer = n % 3 + answer;
+                n = n / 3;
             }
-
-            return answer;
         }
+        return answer;
     }
 }
