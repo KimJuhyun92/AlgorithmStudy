cb6081b813dd652305e10b4914c83cce1534bccc
KimJuhyun92 <juhyun7793@gmail.com>
2020-03-05 18:27:48 +0900

    N-Queen 수정

--- src/Baekjoon/Backtracking/N_Queen.java
+++ src/Baekjoon/Backtracking/N_Queen.java
@@ -1,32 +1,31 @@
-package Programmers;
+package Baekjoon.Backtracking;
 
 import java.util.Scanner;
 
 public class N_Queen{
+
     static int N;
     static int cols[];
     static int count;
 
     public static void main(String args[]){
-
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
         cols = new int[N];
         back_tracking(0);
         System.out.println(count);
-
     }
 
-    public static void back_tracking(int dept){
+    public static void back_tracking(int depth){
         //모든 행에 퀸이 채워졌을경우 출력
-        if(dept == N){
+        if(depth == N){
             count++;
         }
         else{
             for(int i=0;i<N;i++){
-                cols[dept]=i;
-                if(isPossible(dept)){
-                    back_tracking(dept+1);
+                cols[depth]=i;
+                if(isPossible(depth)){
+                    back_tracking(depth+1);
                 }
             }
         }
